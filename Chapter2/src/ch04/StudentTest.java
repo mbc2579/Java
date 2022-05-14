ba741722ab4b93f7e819d218e6034f64066436fa
mbc2579 <mbc2579@naver.com>
2022-05-14 18:11:06 +0900

    Java

---
+++ Chapter2/src/ch04/StudentTest.java
@@ -0,0 +1,19 @@
+package ch04;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		// Student class 积己磊 角青 抗力
+		
+		Student studentLee = new Student();
+		studentLee.grade = 1;
+		
+		System.out.println(studentLee.showStudentInfo());
+		
+		Student studentKim = new Student(123456, "Kim", 3);
+		System.out.println(studentKim.showStudentInfo());
+
+	}
+
+}
